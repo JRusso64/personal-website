0bee30754ac0a3944998fd7f0c66d6b80f231e99
JRusso64 <101598072+JRusso64@users.noreply.github.com>
2024-05-09 22:21:49 -0400

    Still need to make the pdf display. But added resume page.

---
+++ app/resume/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../../components/NavBar';
+import Resume from '../../components/Resume';
+
+export default function Home() {
+  return (
+    <>
+    <NavBar />
+    <Resume fileUrl="../../documents/ResumeForSite.pdf" />
+    </>
+  );
+}
