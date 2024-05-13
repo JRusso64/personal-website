241218bdfd945eb3ac21fd2957dcaa63e6157c3c
JRusso64 <101598072+JRusso64@users.noreply.github.com>
2024-05-12 20:25:00 -0400

    added page for forum and got ll logo on aslingo page

---
+++ app/projects/forum-website/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../../../components/NavBar';
+import ForumProject from '../../../components/project-components/ForumProject';
+ForumProject
+export default function Home() {
+  return (
+    <>
+    <NavBar />
+    <ForumProject /> 
+    </>
+  );
+}
