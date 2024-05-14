09e601d2ee18c185d7e4a8d3ed2840a165ba23cd
JRusso64 <101598072+JRusso64@users.noreply.github.com>
2024-05-13 23:05:39 -0400

    added pictures to forum project page

--- components/project-components/ForumProject.tsx
+++ components/project-components/ForumProject.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import LinkedInLogo from '../../public/lnlogo.png';
+import LoginPhoto from '../../public/ForumPictures/loginscreen.jpg';
+import RegisterPhoto from '../../public/ForumPictures/registerscreen.jpg'
+import PostsPhoto from '../../public/ForumPictures/posts.jpg'
 
 export default function ASLingo() {
   return (
@@ -9,16 +12,42 @@ export default function ASLingo() {
         <h1 style={{ color: '#0056b3', marginBottom: '20px' }}>ASLingo</h1>
         <p>This is a Forum project that I worked on. It is a regular forum website where you can register and login.</p>
         <p>Along with this you can also</p>
-        <p>Here are some photos of the what the website looks like.</p>
-        <div style={{ marginBottom: '20px' }}>
-            <img src="../../images/aslingo-example.png" alt="ASLingo Image" style={{ width: '100%', maxHeight: '400px', objectFit: 'cover' }} />
-        </div>
 
-        <p>Check out the <a href="https://github.com/JRusso64/HackRU23" target="_blank" rel="noopener noreferrer">GitHub repository</a> for more details on the project.</p>
+        <p>Check out the <a href="https://github.com/JRusso64/Forum" target="_blank" rel="noopener noreferrer">GitHub repository</a> for more details on the project.</p>
         
         <a href="https://www.linkedin.com/in/john-russo-993435255/" target="_blank" rel="noopener noreferrer">
           <Image src={LinkedInLogo} width={32} height={32}alt="LinkedIn Profile" />
         </a> 
+
+        <p>Technologies Used</p>
+        <ul>
+          <li>Javascript</li>
+          <li>Nodejs</li>
+          <li>Express</li>
+          <li>PassportJS</li>
+          <li>HTML/CSS</li>
+          <li>MongoDB Atlas</li>
+          <li>Mongoose</li>
+          <li>Boostrap</li>
+        </ul>
+        
+        <p>Here are some photos of the what the website looks like.</p>
+        
+        <h1>Login Page</h1>
+        <div style={{ marginBottom: '20px' }}>
+            <Image src={LoginPhoto} alt="ASLingo Image" style={{ width: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        </div>
+
+        <h1>Register Page</h1>
+        <div style={{ marginBottom: '20px' }}>
+            <Image src={RegisterPhoto} alt="ASLingo Image" style={{ width: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        </div>
+
+        
+        <h1>Posts Page</h1>
+        <div style={{ marginBottom: '20px' }}>
+            <Image src={PostsPhoto} alt="ASLingo Image" style={{ width: '100%', maxHeight: '100%', objectFit: 'cover' }} />
+        </div>
     </>
   );
 }
