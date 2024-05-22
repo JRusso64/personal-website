be765709d5a5a11ee419aa419f23368482535389
JRusso64 <101598072+JRusso64@users.noreply.github.com>
2024-05-21 20:19:31 -0400

    fixed some stuff for deployment

--- app/projects/photo-album/page.tsx
+++ app/projects/photo-album/page.tsx
@@ -1,1 +1,11 @@
 "use client"
+import React from 'react';
+import NavBar from '../../../components/NavBar';
+import TextMessenger from '../../../components/project-components/TextMessenger';
+export default function Home() {
+  return (
+    <>
+    <p>Sorry, I didn't create this page yet!</p>
+    </>
+  );
+}
